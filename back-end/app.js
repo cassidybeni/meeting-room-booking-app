41938a7c3781aeafeca6cc29bbb0653ae03e3d96
cassidybeni <cassidybeni@pursuit.org>
2022-03-30 22:07:53 -0400

    Restart app automatically with nodemon

--- back-end/app.js
+++ back-end/app.js
@@ -9,5 +9,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Meeting Room Booking App");
 });
 
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "Page not found" });
+});
+
 // EXPORT
 module.exports = app;
