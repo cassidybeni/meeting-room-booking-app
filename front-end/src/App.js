b5456e13e6181a31a6ed816024a7ae3f050e18e9
cassidybeni <cassidybeni@pursuit.org>
2022-04-01 19:01:44 -0400

    Use create root instead of browser router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import "./App.css";
+// import NavBar from "./Components/NavBar"
+// import Home from "./Pages/Home";
+// import Bookings from "./Pages/Bookings";
+// import NewRoom from "./Pages/NewRoom";
+// import SingleBooking from "./Pages/SingleBooking"
+// import { apiURL } from "./util/apiURL";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+
+//const API = apiURL()
 
 function App() {
   return (
     <div className="App">
-     
     </div>
   );
 }
